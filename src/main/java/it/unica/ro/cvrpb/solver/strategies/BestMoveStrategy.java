1aa8d990ffa36f17d222a3c99c8d9a9472d3ea3d
Mattia Atzeni <mattia.atzeni@outlook.it>
2018-01-22 21:46:03 +0100

    Debug checks removed

--- src/main/java/it/unica/ro/cvrpb/solver/strategies/BestMoveStrategy.java
+++ src/main/java/it/unica/ro/cvrpb/solver/strategies/BestMoveStrategy.java
@@ -2,7 +2,6 @@ package it.unica.ro.cvrpb.solver.strategies;
 
 import it.unica.ro.cvrpb.solver.moves.CVRPBMove;
 import it.unica.ro.cvrpb.solver.solution.CVRPBSolution;
-import it.unica.ro.cvrpb.solver.solution.CVRPBSolutionChecker;
 
 public abstract class BestMoveStrategy implements CVRPBStrategy {
 
@@ -13,23 +12,7 @@ public abstract class BestMoveStrategy implements CVRPBStrategy {
         CVRPBMove move = findBestMove(solution);
 
         while (move != null) {
-            double preCost = solution.getTotalCost();
-            double advantage = move.evalAdvantage();
             move.apply();
-            double postCost = solution.getTotalCost();
-
-            if (preCost - postCost - advantage > threshold) {
-                System.out.println("Pre: " + preCost);
-                System.out.println("Post: " + postCost);
-                System.out.println("Advantage: " + advantage);
-                System.out.println("Diff: " + (preCost - postCost - advantage));
-                System.out.println(move);
-                throw new RuntimeException("Wrong advantage");
-            }
-            boolean flag = new CVRPBSolutionChecker(solution.getRoutes().get(0).getProblem()).check(solution);
-            if (!flag) {
-                throw new RuntimeException("Oops");
-            }
             move = findBestMove(solution);
         }
     }
