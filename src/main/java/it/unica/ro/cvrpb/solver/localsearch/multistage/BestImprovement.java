adc63157e706e8c01662ce60a3e8259a33c91bf9
Mattia Atzeni <mattia.atzeni@outlook.it>
2018-04-01 11:32:07 +0100

    local search strategies documentation

--- src/main/java/it/unica/ro/cvrpb/solver/localsearch/multistage/BestImprovement.java
+++ src/main/java/it/unica/ro/cvrpb/solver/localsearch/multistage/BestImprovement.java
@@ -7,10 +7,19 @@ import it.unica.ro.cvrpb.solver.moves.MoveOperator;
 import it.unica.ro.cvrpb.solver.solution.CVRPBSolution;
 import it.unica.ro.cvrpb.solver.solution.CVRPBSolutionNodeIterator;
 
+/**
+ * The BestImprovement class represents a best improvement local search strategy.
+ * This strategy evaluates all possible moves and applies the one yielding the best improvement of the objective value.
+ * @param <T>
+ */
 public abstract class BestImprovement<T extends MoveOperator> implements LocalSearchStrategy {
 
     private double threshold = 1e-6;
 
+    /**
+     * Minimizes the given solution iteratively applying this strategy to all nodes
+     * @param solution the initial configuration
+     */
     @Override
     public void minimize(CVRPBSolution solution) {
         boolean flag;
@@ -19,6 +28,11 @@ public abstract class BestImprovement<T extends MoveOperator> implements LocalSe
         } while (flag);
     }
 
+    /**
+     * Applies the strategy to all nodes in the given configuration
+     * @param solution the given configuration
+     * @return true if the solution has been changed, false otherwise
+     */
     private boolean processAllNodes(CVRPBSolution solution) {
         boolean flag = false;
 
@@ -39,19 +53,38 @@ public abstract class BestImprovement<T extends MoveOperator> implements LocalSe
         return flag;
     }
 
+    /**
+     * Returns the threshold used to compare the gains of two moves and select the best
+     * @return the threshold used to compare the gains of the moves
+     */
     public double getThreshold() {
         return threshold;
     }
 
+    /**
+     * Sets the threshold used to compare the gains og the moves
+     * @param threshold the new threshold value
+     */
     public void setThreshold(double threshold) {
         if (threshold < 0) {
-            throw new IllegalArgumentException("Threshold must be greater than 0");
+            throw new IllegalArgumentException("Threshold cannot be lower than 0");
         }
         this.threshold = threshold;
     }
 
+    /**
+     * Finds the best move to be applied to the given node in the given configuration
+     * @param solution the given solution
+     * @param node the node involved in the best move
+     * @return the best move for the given node in the specified solution
+     */
     public abstract T findBestMove(CVRPBSolution solution, CVRPBSolutionNodeIterator node);
 
+    /**
+     * Validates the parameters to check that they are not null
+     * @param solution a feasible solution of a vehicle routing problem
+     * @param node a node in the specified solution
+     */
     protected void validateNotNull(CVRPBSolution solution, CVRPBSolutionNodeIterator node) {
         if (solution == null) {
             throw new IllegalArgumentException("Solution cannot be null");
