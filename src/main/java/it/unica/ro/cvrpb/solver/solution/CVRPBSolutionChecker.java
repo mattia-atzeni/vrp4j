2f56b26b7b67106b101ff2b1d501af9bbf5a9cd5
Mattia Atzeni <mattia.atzeni@outlook.it>
2018-04-07 12:53:56 +0100

    solution documentatoin

--- src/main/java/it/unica/ro/cvrpb/solver/solution/CVRPBSolutionChecker.java
+++ src/main/java/it/unica/ro/cvrpb/solver/solution/CVRPBSolutionChecker.java
@@ -7,23 +7,41 @@ import it.unica.ro.cvrpb.model.Route;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The CVRPBSolutionChecker class allows to check that a CVRPBSolution fulfills all the
+ * constrains of a CVRPBProblem
+ */
 public class CVRPBSolutionChecker {
-    private CVRPBProblem instance;
+    private CVRPBProblem problem;
 
-    public CVRPBSolutionChecker(CVRPBProblem instance) {
-        this.instance = instance;
+    /**
+     * Creates a solution checker for a specified problem
+     * @param problem the problem this checker relates to
+     */
+    public CVRPBSolutionChecker(CVRPBProblem problem) {
+        this.problem = problem;
     }
 
-    public boolean check(CVRPBSolution routes) {
-        return checkAllCustomersVisited(routes) &&
-                checkCapacity(routes) &&
-                checkCustomerPath(routes) &&
-                checkNumberOfVehicles(routes);
+    /**
+     * Checks that the specified solution fulfills all the constraints
+     * @param solution a configuration for a Vehicle Routing Problem
+     * @return true if the solution is feasible, false otherwise
+     */
+    public boolean check(CVRPBSolution solution) {
+        return checkAllCustomersVisited(solution) &&
+                checkCapacity(solution) &&
+                checkCustomerPath(solution) &&
+                checkNumberOfVehicles(solution);
     }
 
-    private boolean checkAllCustomersVisited(CVRPBSolution routes) {
-        List<Customer> customers = instance.getCustomers();
-        List<Customer> visitedCustomers = routes.stream()
+    /**
+     * Verifies that each customer is visited by exactly one route
+     * @param solution a solution to a vehicle routing problem
+     * @return true if each customer is visited by exactly one route, false otherwise
+     */
+    private boolean checkAllCustomersVisited(CVRPBSolution solution) {
+        List<Customer> customers = problem.getCustomers();
+        List<Customer> visitedCustomers = solution.stream()
                 .flatMap(r -> r.getCustomers().stream())
                 .collect(Collectors.toList());
 
@@ -31,10 +49,21 @@ public class CVRPBSolutionChecker {
                visitedCustomers.containsAll(customers);
     }
 
-    private boolean checkCapacity(CVRPBSolution routes) {
-        return routes.stream().allMatch(this::checkCapacity);
+    /**
+     * Checks that all the routes in the provided solution
+     * do not exceed the maximum capacity
+     * @param solution a solution to a VRP
+     * @return true if each route in the provided solution do not exceed the maximum capcacity, false otherwise
+     */
+    private boolean checkCapacity(CVRPBSolution solution) {
+        return solution.stream().allMatch(this::checkCapacity);
     }
 
+    /**
+     * Checks the capacity of the specified route
+     * @param route a route
+     * @return true if the route do not exceed the maximum capacity, false otherwise
+     */
     private boolean checkCapacity(Route route) {
         int deliveryLoad = route.getDeliveryLoad();
         int actualDeliveryLoad = route.getLinehaulCustomers().stream()
@@ -52,15 +81,32 @@ public class CVRPBSolutionChecker {
                 deliveryLoad <= capacity;
     }
 
+    /**
+     * Checks that the specified route has at least one linehaul customer
+     * @param route a route
+     * @return true if the specified route has at least one linehaul customer, false otherwise
+     */
     private boolean checkCustomerPath(Route route) {
         return route.getLinehaulCustomers().size() > 0;
     }
 
-    private boolean checkCustomerPath(CVRPBSolution routes) {
-        return routes.stream().allMatch(this::checkCustomerPath);
+    /**
+     * Checks that each route has at least one linehaul customer
+     * @param solution a solution to a VRP
+     * @return true if each route has at least one linehaul customer, false otherwise
+     */
+    private boolean checkCustomerPath(CVRPBSolution solution) {
+        return solution.stream().allMatch(this::checkCustomerPath);
     }
 
-    private boolean checkNumberOfVehicles(CVRPBSolution routes) {
-        return instance.getNumberOfVehicles() == routes.size();
+    /**
+     * Checks that each vehicle performs exactly one route
+     * @param solution a solution to a VRP
+     * @return true if the number of routes is equal to the number of vehicles
+     * and no route is empty, false otherwise
+     */
+    private boolean checkNumberOfVehicles(CVRPBSolution solution) {
+        return problem.getNumberOfVehicles() == solution.size() &&
+                solution.stream().filter(r -> r.size() <= 2).count() == 0;
     }
 }
